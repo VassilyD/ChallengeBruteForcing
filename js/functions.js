4035a56fc46c0bb8a176e7ad0ad5ba03267af4b3
Vassily Dubois <vassily.dubois@gmail.com>
2017-09-10 19:13:34 +0200

    les trois méthodes fonctionnent simultanément!

--- js/functions.js
+++ js/functions.js
@@ -8,9 +8,12 @@ function getDico() {
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			dico = this.responseText.split("\n");
+			testOneByOne();
+			testDichotomie();
+			testDichotomie2();
 		}
 	};
-	xhttp.open('GET', 'data/dico.txt', false);
+	xhttp.open('GET', 'data/dico.txt', true);
 	xhttp.send();
 }
 
@@ -48,51 +51,126 @@ function testPwd(wDico) {
 
 function formatResult(wDico) {
 	var texte = '<p>Mot de passe : ' + wDico.reponse + '<br>';
-	texte += 'Trouvé en ' + wDico.nbTest + ' essais!<br>';
-	texte += '(' + Math.floor((Date.now() - wDico.dateStart) / 1000) + ' secondes)</p>';
-	//dateStart = 0;
-	//nbTest = 0;
-	//isGood = false;
-	//reponse = '';
+	if(wDico.reponse != 'Not found') {
+		texte += 'Trouvé en ' + wDico.nbTest + ' essais!<br>';
+		texte += '(' + Math.floor((Date.now() - wDico.dateStart) / 1000) + ' secondes)</p>';
+	}
+	console.log(wDico);
 	return texte;
 }
 
-function testOneByOneFin(wDico) {
-	wDico.dom.html(formatResult(wDico));
-}
 function testOneByOneStep(wDico) {
-	wDico.reponse = wDico.d[wDico.i];
-	testPwd(wDico);
+	if(wDico.nbTest < dico.length) {
+		wDico.reponse = wDico.d[wDico.i];
+		testPwd(wDico);
+	}
+	else {
+		wDico.reponse = 'Not found';
+		wDico.dom.html(formatResult(wDico));
+	}
 }
 // Teste tous les mots du dictionnaire un par un
 // Renvoie le mot trouvé, sinon renvoie 'not found'
 function testOneByOne() {
-	var wDico = {d: dico.slice(0), i: 0, dateStart: Date.now(), isGood: false, nbTest: 0, reponse: '', dom: $('#resultat1'), next: testOneByOneStep};
+	var wDico = {d: dico.slice(0), 
+				 i: 0, dateStart: Date.now(), 
+				 isGood: false, 
+				 nbTest: 0, 
+				 reponse: '', 
+				 dom: $('#resultat1'), 
+				 next: testOneByOneStep};
 	testOneByOneStep(wDico);
 }
 
 
-
-function testDichotomieFin(wDico) {
-	$('#resultat2').html(formatResult(wDico));
-}
 function testDichotomieStep(wDico) {
-	if(wDico.i >= wDico.d.length) wDico.i = 0;
-	var m = Math.floor(wDico.d[wDico.i].length / 2); // Recupere l'indice central du sous tableau courant
-	wDico.reponse = wDico.d[wDico.i][m];
-	var tmp = [wDico.d[wDico.i].slice(0, m)].concat([wDico.d[wDico.i].slice(m + 1)]); // Divise le sous tableau en deux
-	tmp = tmp.concat(wDico.d.slice(wDico.i + 1)); // Ajoute les éléments restant après le sous tableau en cours
-	wDico.d = wDico.d.slice(0, wDico.i).concat(tmp);// Ajoute les éléments restant avant le sous tableau en cours
-	wDico.i++; // on à dédoubler un élément donc il faut augmenter iWorking pour le prendre en compte
-	testPwd(wDico);
+	if(wDico.nbTest < dico.length) {
+		if(wDico.i >= wDico.d.length) wDico.i = 0;
+		var m = Math.floor(wDico.d[wDico.i].length / 2); // Recupere l'indice central du sous tableau courant
+		wDico.reponse = wDico.d[wDico.i][m];
+		var tmp = [wDico.d[wDico.i].slice(0, m)].concat([wDico.d[wDico.i].slice(m + 1)]); // Divise le sous tableau en deux en excluant l'élément du milieu
+		tmp = tmp.concat(wDico.d.slice(wDico.i + 1)); // Ajoute les éléments restant après le sous tableau en cours
+		wDico.d = wDico.d.slice(0, wDico.i).concat(tmp);// Ajoute les éléments restant avant le sous tableau en cours
+		wDico.i++; // on à dédoubler un élément donc il faut augmenter iWorking pour le prendre en compte
+		testPwd(wDico);
+	}
+	else {
+		wDico.reponse = 'Not found';
+		wDico.dom.html(formatResult(wDico));
+	}
 }
 // Teste le mot central récursivement
 // Renvoie le mot trouvé, sinon renvoie 'not found'
 function testDichotomie() {
-	var wDico = {d: [dico.slice(0)], i: 0, dateStart: Date.now(), isGood: false, nbTest: 0, reponse: '', dom: $('#resultat2'), next: testDichotomieStep};
+	var wDico = {d: [dico.slice(0)], 
+				 i: 0, 
+				 dateStart: Date.now(), 
+				 isGood: false, 
+				 nbTest: 0, 
+				 reponse: '', 
+				 dom: $('#resultat2'), 
+				 next: testDichotomieStep};
 	testDichotomieStep(wDico);
 }
 
+
+function testDichotomie2Step(wDico) {
+	if(wDico.nbTest < dico.length) {
+		if(wDico.i >= wDico.reponses.length) {
+			if(wDico.d[wDico.iTable].length > 3) {
+				var m = Math.floor(wDico.d[wDico.iTable].length / 2); // Recupere l'indice central du sous tableau courant
+				var tmp = [wDico.d[wDico.iTable].slice(1, m)].concat([wDico.d[wDico.iTable].slice(m + 1, wDico.d[wDico.iTable].length - 1)]); // Divise le sous tableau en deux en excluant les éléments début/milieu/fin
+				tmp = tmp.concat(wDico.d.slice(wDico.iTable + 1)); // Ajoute les éléments restant après le sous tableau en cours
+				wDico.d = wDico.d.slice(0, wDico.iTable).concat(tmp);// Ajoute les éléments restant avant le sous tableau en cours
+				wDico.iTable += 2; // Pour passer au tableau suivante il faut aussi passer le nouveau sous tableau créé
+			}
+			else {
+				wDico.d = wDico.d.slice(0, wDico.iTable).concat(wDico.d.slice(wDico.iTable + 1));
+				// Cette fois ci on supprime le sous tableau en cours donc ni besoin de passer le nouveau ni meme besoin de passer au suivant car le suivant à desormais l'id de celui sur lequel on travaillait
+			}
+			if(wDico.iTable >= wDico.d.length) wDico.iTable = 0;
+			wDico.i = 0;
+		}
+		if(wDico.i == 0) {
+			wDico.reponses = [];
+			if(wDico.d[wDico.iTable].length >= 3) {
+				wDico.reponses.push(wDico.d[wDico.iTable][0]); // Premier élément du sous tableau
+				wDico.reponses.push(wDico.d[wDico.iTable][Math.floor(wDico.d[wDico.iTable].length / 2)]); // Élément central du sous tableau
+				wDico.reponses.push(wDico.d[wDico.iTable][wDico.d[wDico.iTable].length - 1]); // Élément final du sous tableau
+			}
+			else if(wDico.d[wDico.iTable].length == 2) {
+				wDico.reponses.push(wDico.d[wDico.iTable][0]);
+				wDico.reponses.push(wDico.d[wDico.iTable][1]);
+			}
+			else if (wDico.d[wDico.iTable].length == 1) {
+				wDico.reponses.push(wDico.d[wDico.iTable][0]);
+			}
+		}
+		wDico.reponse = wDico.reponses[wDico.i];
+		//wDico.i++; // on à dédoubler un élément donc il faut augmenter iWorking pour le prendre en compte
+		testPwd(wDico);
+	}
+	else {
+		wDico.reponse = 'Not found';
+		wDico.dom.html(formatResult(wDico));
+	}
+}
+// Teste le mot central récursivement
+// Renvoie le mot trouvé, sinon renvoie 'not found'
+function testDichotomie2() {
+	var wDico = {d: [dico.slice(0)],
+				 i: 0, 
+				 dateStart: Date.now(),
+				 isGood: false,
+				 nbTest: 0,
+				 reponse: '', 
+				 reponses: [dico[0], dico[Math.floor(dico.length / 2)], dico[dico.length - 1]], 
+				 iTable: 0, 
+				 dom: $('#resultat3'), 
+				 next: testDichotomie2Step};
+	testDichotomie2Step(wDico);
+}
+/*
 // Teste les mots du début, central et de la fin récursivement
 // Renvoie le mot trouvé, sinon renvoie 'not found'
 function testDichotomie2() {
@@ -120,3 +198,4 @@ function testDichotomie2() {
 	}
 	return (isGood)?reponseTmp:'not found';
 }
+*/
